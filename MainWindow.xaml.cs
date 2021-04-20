e4d3dc7a3d5d46c501f15ec78f5c079fec9ead59
NameIsVamsi <72161057+NameIsVamsi@users.noreply.github.com>
2021-04-20 19:37:12 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ShoppingApp_DS_VK_JMK_SNA.ViewModels;
+
+namespace ShoppingApp_DS_VK_JMK_SNA
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //private ProductViewModel productViewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext =  new ProductViewModel();
+
+        }
+    }
+}
---
+++ ManagerWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ShoppingApp_DS_VK_JMK_SNA.Entities;
+using ShoppingApp_DS_VK_JMK_SNA.ViewModels;
+
+namespace ShoppingApp_DS_VK_JMK_SNA
+{
+    /// <summary>
+    /// Interaction logic for ManagerWindow.xaml
+    /// </summary>
+    public partial class ManagerWindow : Window
+    {
+        public ManagerWindow()
+        {
+            InitializeComponent();
+            DataContext = new ProductViewModel();
+        }
+
+       
+    }
+}
